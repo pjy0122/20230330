be1988ce87bb8aff62b37a5e6ed6155a418d2aea
pjy0122 <pzw0122@gmail.com>
2023-03-30 13:51:35 +0900

    git

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 function App() {
   return (
     <div>
-      <h1>Hello, React!</h1>
-      <h2>My Name is pjy0122</h2>
-      <h3>My Favorite drink is Zero Coke</h3>
+      <h1 className="inline-block bg-color-pink p-12 radius-12">
+        Hello, React!
+      </h1>
+      <h2 className="inline-block bg-color-aqua p-12 radius-12">
+        My Name is pjy0122
+      </h2>
+      <h3 className="inline-block bg-color-deepskyblue p-12 radius-12">
+        My Favorite drink is Zero Coke 🥤
+      </h3>
     </div>
   );
 }
