9e1036eb6bf6afbfbeaa2ee0948d0cd88cbeb424
pjy0122 <pzw0122@gmail.com>
2023-03-30 10:33:40 +0900

    Favorite

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return <h1>Hello, React!</h1>;
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <h2>My Name is pjy0122</h2>
+      <h3>My Favorite drink is Zero Coke</h3>
+    </div>
+  );
 }
 
 export default App;
